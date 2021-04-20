0bfd683cef0bedc6799f83f075ac45d8692e1d0e
langdag <lucky10cents@gmail.com>
2021-04-20 19:38:09 +0300

    fetch images

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
+const fetch = require('node-fetch');
 
 const app = express();
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-   res.send('<h1>Hello World!!!</h1>');
+    async function getImages() {
+        const images = await fetch(
+            "https://api.giphy.com/v1/gifs/search?api_key=RowCuvnkiP1yew6ix6ogw7ckHTUOJMwS&q=cheese&limit=25&offset=0&lang=en"
+        );
+        let response = await images.json();
+        console.log(response);
+    }
+
+    getImages();
+    res.render('home');
 });
 
 const PORT = process.env.PORT || 5000;
