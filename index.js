f46b15b7a6814eb43c5ae3beb9c05c2adbe83cbe
langdag <lucky10cents@gmail.com>
2021-04-21 14:05:03 +0300

    refactor api url

--- index.js
+++ index.js
@@ -8,23 +8,30 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+const apiUrl = {
+    base: 'https://api.giphy.com/v1/gifs/search',
+    key: 'RowCuvnkiP1yew6ix6ogw7ckHTUOJMwS',
+    query: 'ball',
+    limit: 25
+}
+
+
 app.get('/', (req, res) => {
     async function getImages() {
         const images = await fetch(
-            "https://api.giphy.com/v1/gifs/search?api_key=RowCuvnkiP1yew6ix6ogw7ckHTUOJMwS&q=cheese&limit=25&offset=0&lang=en"
+            `${apiUrl.base}?api_key=${apiUrl.key}&q=${apiUrl.query}&limit=${apiUrl.limit}&offset=0&lang=en`
         );
         let response = await images.json();
-        let sorted_data = response["data"].sort(function (a, b) {
+        let sortedData = response["data"].sort(function (a, b) {
             return a.rating.localeCompare(b.rating);
         });
-        saveDataToFile(sorted_data, function(err) {
+        saveDataToFile(sortedData, function(err) {
             if (err) {
                 throw err;
             }
             console.log("JSON data is saved.");
         });
     }
-
     getImages();
 
     readFile('./images.json', (err, data) => {
@@ -37,8 +44,8 @@ app.get('/', (req, res) => {
 });
 
 function saveDataToFile(data, callback) {
-    first_ten_elements = data.slice(0,10)
-    fs.writeFile('./images.json', JSON.stringify(first_ten_elements, null, 2), callback);
+    firstTenElements = data.slice(0,10)
+    fs.writeFile('./images.json', JSON.stringify(firstTenElements, null, 2), callback);
 }
 
 function readFile(path, callback) {
